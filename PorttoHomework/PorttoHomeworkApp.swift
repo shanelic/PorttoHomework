572eb8dfcf51cb6599ddc2e94413cf226f3c60a1
shanelic_ <l83112006@me.com>
2023-09-12 23:28:31 +0800

    Initial Commit

---
+++ PorttoHomework/PorttoHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorttoHomeworkApp.swift
+//  PorttoHomework
+//
+//  Created by Shane Li on 2023/9/12.
+//
+
+import SwiftUI
+
+@main
+struct PorttoHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
