945b1bafb293e50f343c3b81a9dc445f0861392c
shanelic_ <me@shanelic.io>
2023-09-14 22:58:59 +0800

    feat: get NFTs from Opensea via Web3Actor

--- PorttoHomework/MainViewModel.swift
+++ PorttoHomework/MainViewModel.swift
@@ -6,18 +6,36 @@
 //
 
 import Foundation
+import Combine
 import Web3Actor
 import Web3
 
 class MainViewModel: ObservableObject {
     
+    @Published var nfts: Set<Opensea.NFT> = []
+    
     private var cancellables: [AnyCancellable] = []
     
     private let address = EthereumAddress(hexString: "0x85fD692D2a075908079261F5E351e7fE0267dB02")!
+    private var nftCursor: String?
     
     init() {
         Task {
             await Web3Actor.shared.initialize(.EthereumGoerli, openseaApiKey: "c823fdee93814f7abd5492604697e9c8")
+            await Web3Actor.shared.$nfts.sink(receiveValue: { nfts in
+                self.nfts = nfts
+            }).store(in: &cancellables)
+        }
+    }
+    
+    public func getNfts() {
+        Task {
+            do {
+                guard nfts.isEmpty || nftCursor != nil else { return }
+                nftCursor = try await Web3Actor.shared.getNFTs(of: address, nextCursor: nftCursor)
+            } catch {
+                print("::: error occurred fetching NFTs with cursor \(nftCursor ?? "nil"):", error)
+            }
         }
     }
 }
