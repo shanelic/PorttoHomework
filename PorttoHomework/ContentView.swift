4789c0400323a8988b33a33ee9bb20feb1d4861b
shanelic_ <me@shanelic.io>
2023-09-14 22:59:40 +0800

    feat: implement view model into view

--- PorttoHomework/ContentView.swift
+++ PorttoHomework/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel = MainViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
